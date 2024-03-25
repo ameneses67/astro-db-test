5b0b32c4a2c454f92e26892f57b719a68142d911
Alfonso Meneses <ameneses67@gmail.com>
2024-03-25 12:57:14 -0600

    Billboard

--- db/config.ts
+++ db/config.ts
@@ -1,70 +1,72 @@
 import { column, defineDb, defineTable, NOW } from "astro:db";
 
 const Category = defineTable({
-	columns: {
-		id: column.number({ primaryKey: true, unique: true }),
-		name: column.text(),
-		image: column.text(),
-	},
+  columns: {
+    id: column.number({ primaryKey: true, unique: true }),
+    name: column.text({ unique: true }),
+    description: column.text(),
+    image: column.text(),
+  },
 });
 
 const Subcategory = defineTable({
-	columns: {
-		id: column.number({ primaryKey: true, unique: true }),
-		name: column.text(),
-		image: column.text(),
-		categoryId: column.number({ references: () => Category.columns.id }),
-	},
+  columns: {
+    id: column.number({ primaryKey: true, unique: true }),
+    name: column.text({ unique: true }),
+    description: column.text(),
+    image: column.text(),
+    categoryId: column.number({ references: () => Category.columns.id }),
+  },
 });
 
 const Brand = defineTable({
-	columns: {
-		id: column.number({ primaryKey: true, unique: true }),
-		name: column.text(),
-	},
+  columns: {
+    id: column.number({ primaryKey: true, unique: true }),
+    name: column.text(),
+  },
 });
 
 const Size = defineTable({
-	columns: {
-		id: column.number({ primaryKey: true, unique: true }),
-		code: column.text(),
-		description: column.text(),
-	},
+  columns: {
+    id: column.number({ primaryKey: true, unique: true }),
+    code: column.text(),
+    description: column.text(),
+  },
 });
 
 const Color = defineTable({
-	columns: {
-		id: column.number({ primaryKey: true, unique: true }),
-		code: column.text(),
-		description: column.text(),
-	},
+  columns: {
+    id: column.number({ primaryKey: true, unique: true }),
+    code: column.text(),
+    description: column.text(),
+  },
 });
 
 const Product = defineTable({
-	columns: {
-		id: column.number({ primaryKey: true, unique: true }),
-		name: column.text(),
-		description: column.text(),
-		brandId: column.number({
-			references: () => Brand.columns.id,
-			optional: true,
-		}),
-		categoryId: column.number({ references: () => Category.columns.id }),
-		subcategoryId: column.number({ references: () => Subcategory.columns.id }),
-		price: column.number(),
-		sizeId: column.number({
-			references: () => Size.columns.id,
-			optional: true,
-		}),
-		colorId: column.number({
-			references: () => Color.columns.id,
-			optional: true,
-		}),
-		image: column.text(),
-		published: column.date({ default: NOW }),
-	},
+  columns: {
+    id: column.number({ primaryKey: true, unique: true }),
+    name: column.text(),
+    description: column.text(),
+    brandId: column.number({
+      references: () => Brand.columns.id,
+      optional: true,
+    }),
+    categoryId: column.number({ references: () => Category.columns.id }),
+    subcategoryId: column.number({ references: () => Subcategory.columns.id }),
+    price: column.number(),
+    sizeId: column.number({
+      references: () => Size.columns.id,
+      optional: true,
+    }),
+    colorId: column.number({
+      references: () => Color.columns.id,
+      optional: true,
+    }),
+    image: column.text(),
+    published: column.date({ default: NOW }),
+  },
 });
 
 export default defineDb({
-	tables: { Category, Subcategory, Size, Color, Product, Brand },
+  tables: { Category, Subcategory, Size, Color, Product, Brand },
 });
